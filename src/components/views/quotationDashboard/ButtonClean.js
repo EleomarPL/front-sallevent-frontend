67f1ba4537ce15e13c751ac1a0eb0e618f16f6ff
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-08-15 12:39:54 -0500

    ButtonClean component of quote structure, added

---
+++ src/components/views/quotationDashboard/ButtonClean.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+
+import Quotation from '../../../contexts/Quotation';
+
+const ButtonClean = () => {
+  
+  const {quotationData, setQuotationData} = useContext(Quotation);
+
+  const handleReset = () => {
+    const {listServices} = quotationData;
+    setQuotationData({
+      listServices,
+      typeEvent: '',
+      starttime: {
+        time: '',
+        timetable: 'am'
+      },
+      finaltime: {
+        time: '',
+        timetable: 'am'
+      }
+    });
+  };
+  return (
+    <button type="reset" className="btn btn-dark btn-sm"
+      form="formQuotation" onClick={ handleReset }
+    >
+      Limpiar
+    </button>
+  );
+};
+
+export default ButtonClean;
