8877ad8136445b6148fa2f25b6b100c9e9756edc
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-08-26 17:58:50 -0500

    function to create-reservation was added in reservation API

--- src/services/apis/reservation.js
+++ src/services/apis/reservation.js
@@ -16,3 +16,14 @@ export const getReservations = async({token}) => {
   let response = await axios.get(`${baseAPI}/get-reservations`, config);
   return response;
 };
+export const createReservation = async({listSelectedServices, timeStart, timeEnd, typeEvent, dateYYMMDD, token}) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
+  let response = await axios.post(`${baseAPI}/create-reservation`, {
+    listSelectedServices, timeStart, timeEnd, typeEvent, dateYYMMDD
+  }, config);
+  return response;
+};
