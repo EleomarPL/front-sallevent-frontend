d721d128c86f6dba85d978e3c1daf26e9cea83ff
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-08-27 14:04:37 -0500

    base view of the Reservation page, without the buttons

---
+++ src/components/views/user/StatusReservation.js
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+
+import ReservationUser from '../../../contexts/ReservationUser';
+
+const StatusReservation = () => {
+  const {dataReservation} = useContext(ReservationUser);
+  let dataReservationToRender = {
+    'Horas contratadas': dataReservation.timeEnd - dataReservation.timeStart,
+    'Hora Inicio': `${dataReservation.timeStart}:00:00 horas`,
+    'Hora Final': `${dataReservation.timeEnd}:00:00 horas`
+  };
+  return (
+    <div className="col-md-12">
+      <div className="col-md-12 d-flex flex-wrap justify-content-center mb-4">
+        <h1 style={ {fontSize: 'calc(1.325rem + .9vw)', marginRight: '0.5rem'} }>Evento: </h1>
+        <h2>{ dataReservation.typeEvent }</h2>
+      </div>
+      <div className="table-responsive" style={ {maxWidth: '90vw'} }>
+        <table className="w-100">
+          <tbody>
+            { dataReservationToRender &&
+          Object.keys(dataReservationToRender).map(key =>
+            <tr
+              key={ key }
+            >
+              <td style={ {paddingBottom: '1rem'} }> { key }: </td>
+              <td style={ {paddingBottom: '1rem'} } className="text-danger">
+                { dataReservationToRender[key] }
+              </td>
+            </tr>
+          )
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default StatusReservation;
--- src/pages/my/Reservation.js
+++ src/pages/my/Reservation.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import DataUser from '../../components/views/user/DataUser';
+import StatusReservation from '../../components/views/user/StatusReservation';
+import ReservationUser from '../../contexts/ReservationUser';
 
 const Reservation = () => {
+  const {dataReservation} = useContext(ReservationUser);
   return (
-    <p>Estado de la reservacion</p>
+    <section className="container-fluid p-md-3">
+      <div className="col-md-12 border border-dark">
+        <div className="d-flex justify-content-around align-items-center">
+          <div className="col-md-5 p-2 mb-2">
+            <StatusReservation />
+          </div>
+          <div className="col-md-5 p-2 mb-2">
+            <h2 className="col-md-12 text-center mb-4">Datos del cliente</h2>
+            <DataUser />
+          </div>
+        </div>
+        <div className="mt-4 mx-md-3">
+          <p>
+            Total a pagar:
+            <span className="text-danger" style={ {marginLeft: '1rem'} }>
+              { dataReservation.priceTotal }
+            </span>
+          </p>
+          <p>
+            Estado:
+            <span className="text-danger" style={ {marginLeft: '1rem'} }>
+              { dataReservation.statusReservation === 0 ? 'En espera' : 'Confirmado' }
+            </span>
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 
