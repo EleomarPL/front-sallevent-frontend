4557f40f6613c4be2dc011f4d63d13c2b28839d6
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-09-03 10:59:03 -0500

    searcher layout added to Reservations Page

---
+++ src/components/views/admin/SearcherReservations.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SearcherReservations = ({reservationsToSearcher, setReservationsToSearcher}) => {
+  return (
+    <div className="input-group mb-3">
+      <label className="input-group-text" htmlFor="searchreservations">Options</label>
+      <select className="form-select" id="searchreservations"
+        value={ reservationsToSearcher }
+        onChange={ (evt) => setReservationsToSearcher(evt.target.value) }
+      >
+        <option value="all">Todos</option>
+        <option value="confirmed">Confirmado</option>
+        <option value="notConfirmed">Por confirmar</option>
+      </select>
+      <button
+        type="button"
+        className="btn btn-success"
+      >
+        Buscar
+      </button>
+    </div>
+  );
+};
+
+SearcherReservations.propTypes = {
+  reservationsToSearcher: PropTypes.string.isRequired,
+  setReservationsToSearcher: PropTypes.func.isRequired
+};
+
+export default SearcherReservations;
--- src/pages/admin/Reservations.js
+++ src/pages/admin/Reservations.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import SearcherReservations from '../../components/views/admin/SearcherReservations';
 
 import StatusReservations from '../../components/views/admin/StatusReservations';
 import useReservation from '../../hooks/useReservation';
 
 const Reservations = () => {
   const [reservations, setReservations] = useState([]);
+  const [reservationsToSearcher, setReservationsToSearcher] = useState('all');
   const {getAllReservations} = useReservation();
 
   useEffect(() => {
@@ -18,6 +20,10 @@ const Reservations = () => {
     <section className="container-fluid">
       <h1 style={ {fontSize: '1.8rem'} } className="text-center">Reservaciones</h1>
       <StatusReservations reservations={ reservations } />
+      <SearcherReservations
+        reservationsToSearcher={ reservationsToSearcher }
+        setReservationsToSearcher={ setReservationsToSearcher }
+      />
     </section>
   );
 };
