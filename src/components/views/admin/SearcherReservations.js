f273225d44182b8de2991b8cf21713e7f6de9843
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-09-03 11:47:44 -0500

    Search button functionality on the Reservations page

--- src/components/views/admin/SearcherReservations.js
+++ src/components/views/admin/SearcherReservations.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const SearcherReservations = ({reservationsToSearcher, setReservationsToSearcher}) => {
+  const [selectVale, setSelectValue] = useState(reservationsToSearcher);
+  
   return (
     <div className="input-group mb-3">
       <label className="input-group-text" htmlFor="searchreservations">Options</label>
       <select className="form-select" id="searchreservations"
-        value={ reservationsToSearcher }
-        onChange={ (evt) => setReservationsToSearcher(evt.target.value) }
+        value={ selectVale }
+        onChange={ (evt) => setSelectValue(evt.target.value) }
       >
         <option value="all">Todos</option>
         <option value="confirmed">Confirmado</option>
         <option value="notConfirmed">Por confirmar</option>
       </select>
       <button
+        onClick={ () => setReservationsToSearcher(selectVale) }
         type="button"
         className="btn btn-success"
       >
