e9f8b22940866fab30dc6d8fbc78ec8be40ed04c
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-09-03 10:45:56 -0500

    functionality get all reservations added in Reservation Page

--- src/pages/admin/Reservations.js
+++ src/pages/admin/Reservations.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import StatusReservations from '../../components/views/admin/StatusReservations';
+import useReservation from '../../hooks/useReservation';
 
 const Reservations = () => {
   const [reservations, setReservations] = useState([]);
+  const {getAllReservations} = useReservation();
 
+  useEffect(() => {
+    getAllReservations().then(response => {
+      if (response !== null) {
+        setReservations(response);
+      }
+    });
+  }, []);
   return (
     <section className="container-fluid">
       <h1 style={ {fontSize: '1.8rem'} } className="text-center">Reservaciones</h1>
